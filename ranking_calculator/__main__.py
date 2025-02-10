1538454084edce62ea83e86d7e7049790db79536
RudoAlbelli <rudolf.brus@albelli.com>
2025-02-10 15:18:16 +0100

    Create separate settings file

--- ranking_calculator/__main__.py
+++ ranking_calculator/__main__.py
@@ -4,86 +4,21 @@ from pathlib import Path
 
 import pandas as pd
 
+from ranking_calculator.settings import (
+    AGE_MULTIPLIERS,
+    PLAYER_MULTIPLIERS,
+    POINTS_MAPPING,
+)
+
 DATA_DIRECTORY = Path(__file__).parent / "tournament_data"
-POINTS_MAPPING = {
-    1: 1000,
-    2: 900,
-    3: 800,
-    4: 700,
-    5: 500,
-    6: 500,
-    7: 400,
-    8: 400,
-    9: 300,
-    10: 300,
-    11: 300,
-    12: 300,
-    13: 225,
-    14: 225,
-    15: 225,
-    16: 225,
-    17: 150,
-    18: 150,
-    19: 150,
-    20: 150,
-    21: 150,
-    22: 150,
-    23: 150,
-    24: 150,
-    25: 125,
-    26: 125,
-    27: 125,
-    28: 125,
-    29: 125,
-    30: 125,
-    31: 125,
-    32: 125,
-    33: 100,
-    34: 100,
-    35: 100,
-    36: 100,
-    37: 100,
-    38: 100,
-    39: 100,
-    40: 100,
-    41: 75,
-    42: 75,
-    43: 75,
-    44: 75,
-    45: 75,
-    46: 75,
-    47: 75,
-    48: 75,
-    49: 50,
-    50: 50,
-    51: 50,
-    52: 50,
-    53: 50,
-    54: 50,
-    55: 50,
-    56: 50,
-    57: 25,
-    58: 25,
-    59: 25,
-    60: 25,
-    61: 25,
-    62: 25,
-    63: 25,
-    64: 25,
-}
 
 
 def get_age_multiplier(tournament_date: str) -> float:
     tournament_date = datetime.strptime(tournament_date, "%Y-%m-%d")
     age_in_months = (datetime.now() - tournament_date).days // 30
-    if age_in_months > 24:
-        return 0.0
-    if age_in_months > 18:
-        return 0.4
-    if age_in_months > 12:
-        return 0.6
-    if age_in_months > 6:
-        return 0.8
+    for threshold, age_multiplier in AGE_MULTIPLIERS.items():
+        if age_in_months <= threshold:
+            return age_multiplier
     return 1.0
 
 
@@ -95,12 +30,9 @@ def calculate_points(
         rank_row = ranking[ranking["name"] == player]
         if not rank_row.empty:
             rank = rank_row["rank"].values[0]
-            if rank < 10:
-                multiplier += 0.06
-            elif rank < 20:
-                multiplier += 0.04
-            elif rank < 30:
-                multiplier += 0.02
+            for threshold, player_multiplier in PLAYER_MULTIPLIERS.items():
+                if rank <= threshold:
+                    multiplier += player_multiplier
     base_points = tournament_results["rank"].map(POINTS_MAPPING)
     return base_points * multiplier
 
