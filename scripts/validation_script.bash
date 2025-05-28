9a8c57499b356232bfaf3dc310ae9598861afe8c
RudoAlbelli <rudolf.brus@albelli.com>
2025-05-28 10:30:32 +0200

    Add validation script

---
+++ scripts/validation_script.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set the absolute path to the project root
+PROJECT_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+TARGET_FOLDERS="$PROJECT_ROOT/ranking_calculator $PROJECT_ROOT/tests"
+
+echo "Running mypy..."
+pipenv run mypy $TARGET_FOLDERS
+
+echo "Running pylint..."
+pipenv run pylint $TARGET_FOLDERS
+
+echo "Running ruff..."
+pipenv run ruff $TARGET_FOLDERS
+
+echo "Running pytest..."
+pipenv run pytest $PROJECT_ROOT/tests
+
+echo "All checks passed successfully!"
